ea9faad9c94ce7b635a9503e0fc1ff4e4a062020
Yasmin-Maia <ymaia-do@student.42porto.com>
2025-02-21 20:57:34 +0000

    Add files via upload

--- rotate.c
+++ rotate.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotate.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ymaia-do <ymaia-do@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yasmin <yasmin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 17:30:48 by ymaia-do          #+#    #+#             */
-/*   Updated: 2025/02/14 18:07:54 by ymaia-do         ###   ########.fr       */
+/*   Updated: 2025/02/21 17:40:08 by yasmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,43 @@ void	ra(t_elem **a)
 		last = last->next;
 	last->next = temp;
 	temp->next = NULL;
+	ft_printf("ra\n");
 }
 
 void	rb(t_elem **b)
+{
+	t_elem	*temp;
+	t_elem	*last;
+
+	if(!(*b || (*b)->next))
+		return;
+	temp = *b;
+	*b = (*b)->next;
+	last = *b;
+	while(last->next)
+		last = last->next;
+	last->next = temp;
+	temp->next = NULL;
+	ft_printf("rb\n");
+}
+
+void	ra_no_print(t_elem **a)
+{
+	t_elem	*temp;
+	t_elem	*last;
+
+	if(!(*a || !(*a)->next))
+		return;
+	temp = *a;
+	*a = (*a)->next;
+	last = *a;
+	while(last->next)
+		last = last->next;
+	last->next = temp;
+	temp->next = NULL;
+}
+
+void	rb_no_print(t_elem **b)
 {
 	t_elem	*temp;
 	t_elem	*last;
@@ -46,8 +80,9 @@ void	rb(t_elem **b)
 
 void	rr(t_elem **a, t_elem **b)
 {
-	ra(a);
-	rb(b);
+	ra_no_print(a);
+	rb_no_print(b);
+	ft_printf("rr\n");
 }
 
 /* void print_stack(t_elem *stack)
